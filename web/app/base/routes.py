30a3ddd040ed5b84cbb51ea74e78b899996cf208
ha-yujin <48276686+ha-yujin@users.noreply.github.com>
2020-08-05 02:34:06 +0900

    to simple router

--- web/app/base/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, redirect, request, url_for
-from app.base import blueprint
-
-@blueprint.route('/')
-def route_default():
-    return render_template('index.html')
--- web/run.py
+++ web/run.py
@@ -1,10 +1,17 @@
-from app import create_app
+#from app import create_app
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
-app=create_app()
-
+app = Flask(__name__)
 mysql = MySQL(app)
 
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     try:
