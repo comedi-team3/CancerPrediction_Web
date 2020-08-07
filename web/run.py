82c7a71e808da9cf3eccfedc00271f5c61fbfe15
ha-yujin <48276686+ha-yujin@users.noreply.github.com>
2020-08-07 17:19:55 +0900

    file upload

--- web/run.py
+++ web/run.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 from config import DB_CONFIG
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = DB_CONFIG['host']
-app.config['MYSQL_USER'] = DB_CONFIG['user']
-app.config['MYSQL_PASSWORD'] = DB_CONFIG['password']
-app.config['MYSQL_DB'] = DB_CONFIG['database']
+# app.config['MYSQL_HOST'] = DB_CONFIG['host']
+# app.config['MYSQL_USER'] = DB_CONFIG['user']
+# app.config['MYSQL_PASSWORD'] = DB_CONFIG['password']
+# app.config['MYSQL_DB'] = DB_CONFIG['database']
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 
 @app.route('/')
@@ -34,17 +35,26 @@ def route_template(template):
     return render_template(template)
 
 
-@app.route('/test', methods=['GET','POST'])
-def test():
-    if request.method == 'POST':
-        url = request.form['url']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO image VALUES (%s,%s)",("num2",url))
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      #저장할 경로 + 파일명
+      f.save(secure_filename(f.filename))
+      return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
-        mysql.connection.commit()
 
-        cur.close()
-    return render_template('test.html',url=url)
+# @app.route('/test', methods=['GET','POST'])
+# def test():
+#     if request.method == 'POST':
+#         url = request.form['url']
+#         cur = mysql.connection.cursor()
+#         cur.execute("INSERT INTO image VALUES (%s,%s)",("num2",url))
+
+#         mysql.connection.commit()
+
+#         cur.close()
+#     return render_template('test.html',url=url)
         
 
 if __name__ == "__main__":
